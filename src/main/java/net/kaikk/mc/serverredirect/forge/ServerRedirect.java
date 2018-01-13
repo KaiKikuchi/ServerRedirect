74e172709acd446938686c341ba6aa667f0b1dbd
Kai <kai.kikuchi27@gmail.com>
2018-01-13 19:52:09 +0100

    Version 1.3.7

--- src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
+++ src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
@@ -42,7 +42,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ServerRedirect {
 	public static final String MODID = "serverredirect";
 	public static final String NAME = "ServerRedirect";
-	public static final String VERSION = "1.3.6";
+	public static final String VERSION = "1.3.7";
 
 	// Channel to send messages between server and client... like a request from the server to the client to connect to another server address
 	public static SimpleNetworkWrapper net;
