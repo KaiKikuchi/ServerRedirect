c35c754354f20a252d412592d85d8a83ed449100
Kai <kai.kikuchi27@gmail.com>
2018-06-16 15:04:13 +0200

    Version 1.3.8-BETA1
    Added r=[radius] argument for the command (closes #5)
    Added separate permissions for the command (closes #6)

--- src/main/java/net/kaikk/mc/serverredirect/bukkit/CommandExec.java
+++ src/main/java/net/kaikk/mc/serverredirect/bukkit/CommandExec.java
@@ -4,6 +4,8 @@ import java.util.StringJoiner;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,7 +17,7 @@ public class CommandExec implements CommandExecutor {
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length<2) {
-			sender.sendMessage("Usage: /redirect <address> <PlayerName|PlayerUUID|\"*\">");
+			sender.sendMessage("Usage: /redirect <address> <PlayerName|PlayerUUID|\"r=[radius]\"|\"*\">");
 			sender.sendMessage("Usage: /redirect \"playerslist\"");
 			return false;
 		}
@@ -34,13 +36,34 @@ public class CommandExec implements CommandExecutor {
 			return true;
 		}
 		
-		if (!sender.hasPermission("serverredirect.command.redirect")) {
-			sender.sendMessage(ChatColor.RED + "Permission denied.");
-			return false;
-		}
-		
 		if (args[1].equals("*")) {
+			if (!sender.hasPermission("serverredirect.command.redirect.all")) {
+				sender.sendMessage(ChatColor.RED + "Permission denied.");
+				return false;
+			}
 			ServerRedirect.sendToAll(args[0]);
+		} else if (args[1].startsWith("r=")) {
+			if (!sender.hasPermission("serverredirect.command.redirect.radius")) {
+				sender.sendMessage(ChatColor.RED + "Permission denied.");
+				return false;
+			}
+			final Location loc;
+			if (sender instanceof Player) {
+				loc = ((Player) sender).getLocation();
+			} else if (sender instanceof BlockCommandSender) {
+				loc = ((BlockCommandSender) sender).getBlock().getLocation();
+			} else {
+				sender.sendMessage("Only players and command blocks can use the \"r=\" parameter!");
+				return false;
+			}
+			
+			final int radiusSquared = (int) Math.pow(Integer.valueOf(args[1].substring(2)), 2);
+			
+			for (Player p : Bukkit.getOnlinePlayers()) {
+				if (p.getLocation().getWorld() == loc.getWorld() && p.getLocation().distanceSquared(loc) < radiusSquared) {
+					ServerRedirect.sendTo(p, args[0]);
+				}
+			}
 		} else {
 			Player playerToSend;
 			try {
@@ -51,6 +74,11 @@ public class CommandExec implements CommandExecutor {
 			
 			if (playerToSend == null) {
 				sender.sendMessage("Player "+args[1]+" not found");
+				return false;
+			}
+			
+			if (!sender.hasPermission("serverredirect.command.redirect."+(playerToSend == sender ? "self" : "others"))) {
+				sender.sendMessage(ChatColor.RED + "Permission denied.");
 				return false;
 			}
 			
--- src/main/java/net/kaikk/mc/serverredirect/sponge/CommandExec.java
+++ src/main/java/net/kaikk/mc/serverredirect/sponge/CommandExec.java
@@ -11,6 +11,7 @@ import org.spongepowered.api.command.CommandCallable;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.source.CommandBlockSource;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
@@ -41,13 +42,36 @@ public class CommandExec implements CommandCallable {
 			return CommandResult.success();
 		}
 
-		if (!source.hasPermission("serverredirect.command.redirect")) {
-			source.sendMessage(Text.of(TextColors.RED, "Permission denied"));
-			return CommandResult.empty();
-		}
-		
 		if (args[1].equals("*")) {
+			if (!source.hasPermission("serverredirect.command.redirect.all")) {
+				source.sendMessage(Text.of(TextColors.RED, "Permission denied"));
+				return CommandResult.empty();
+			}
+			
 			ServerRedirect.sendToAll(args[0]);
+		} else if (args[1].startsWith("r=")) {
+			if (!source.hasPermission("serverredirect.command.redirect.radius")) {
+				source.sendMessage(Text.of(TextColors.RED, "Permission denied"));
+				return CommandResult.empty();
+			}
+			
+			final Location<World> loc;
+			if (source instanceof Player) {
+				loc = ((Player) source).getLocation();
+			} else if (source instanceof CommandBlockSource) {
+				loc = ((CommandBlockSource) source).getLocation();
+			} else {
+				source.sendMessage(Text.of("Only players and command blocks can use the \"r=\" parameter!"));
+				return CommandResult.empty();
+			}
+			
+			final int radiusSquared = (int) Math.pow(Integer.valueOf(args[1].substring(2)), 2);
+			
+			for (Player p : Sponge.getServer().getOnlinePlayers()) {
+				if (p.getLocation().inExtent(loc.getExtent()) && p.getLocation().getPosition().distanceSquared(loc.getPosition()) < radiusSquared) {
+					ServerRedirect.sendTo(p, args[0]);
+				}
+			}
 		} else {
 			Optional<Player> playerToSend;
 			try {
@@ -58,6 +82,11 @@ public class CommandExec implements CommandCallable {
 			
 			if (!playerToSend.isPresent()) {
 				source.sendMessage(Text.of("Player "+args[1]+" not found"));
+				return CommandResult.empty();
+			}
+			
+			if (!source.hasPermission("serverredirect.command.redirect."+(playerToSend.get() == source ? "self" : "others"))) {
+				source.sendMessage(Text.of(TextColors.RED, "Permission denied"));
 				return CommandResult.empty();
 			}
 			
@@ -85,10 +114,10 @@ public class CommandExec implements CommandCallable {
 	public Optional<Text> getHelp(CommandSource source) {
 		return Optional.empty();
 	}
-
+	
 	@Override
 	public Text getUsage(CommandSource source) {
-		return Text.of("Usage: /redirect <address> <PlayerName|PlayerUUID|\"*\">", Text.NEW_LINE,
+		return Text.of("Usage: /redirect <address> <PlayerName|PlayerUUID|\"r=[radius]\"|\"*\">", Text.NEW_LINE,
 				"Usage: /redirect \"playerslist\"");
 	}
 }
