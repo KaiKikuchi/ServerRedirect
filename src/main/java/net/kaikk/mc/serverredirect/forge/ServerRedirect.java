71a5fedf9821d16feee8c36475eb1a3c56ff35f7
KaiNoMood <kainomood@gmail.com>
2022-01-18 09:29:23 +0100

    Version 1.4.1

--- src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
+++ src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ServerRedirect {
 	public static final String MODID = "serverredirect";
 	public static final String NAME = "ServerRedirect";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.4.1";
 	public static final Logger LOGGER = LogManager.getLogger();
 	@SideOnly(Side.CLIENT)
 	public static volatile String fallbackServerAddress;
