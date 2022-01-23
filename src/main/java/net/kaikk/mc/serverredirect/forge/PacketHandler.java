b2ba34daf4f8b90dfc52dd89801c58a26035e11d
KaiNoMood <kainomood@gmail.com>
2022-01-24 00:18:28 +0100

    Added announce channel

--- src/main/java/net/kaikk/mc/serverredirect/forge/PacketHandler.java
+++ src/main/java/net/kaikk/mc/serverredirect/forge/PacketHandler.java
@@ -14,11 +14,13 @@ import net.minecraftforge.fml.relauncher.Side;
 public class PacketHandler {
 	public static final SimpleNetworkWrapper REDIRECT_CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel("srvredirect:red");
 	public static final SimpleNetworkWrapper FALLBACK_CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel("srvredirect:fal");
+	public static final SimpleNetworkWrapper ANNOUNCE_CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel("srvredirect:ann");
 	public static final Pattern ADDRESS_PREVALIDATOR = Pattern.compile("^[A-Za-z0-9-_.:]+$"); // allowed characters in a server address
 
 	public static void init() {
 		REDIRECT_CHANNEL.registerMessage(PacketHandler::onRedirectAddressMessage, AddressMessage.class, 0, Side.CLIENT);
 		FALLBACK_CHANNEL.registerMessage(PacketHandler::onFallbackAddressMessage, AddressMessage.class, 0, Side.CLIENT);
+		ANNOUNCE_CHANNEL.registerMessage(PacketHandler::onAnnounceMessage, VoidMessage.class, 0, Side.SERVER);
 	}
 
 	public static IMessage onRedirectAddressMessage(final AddressMessage message, MessageContext ctx) {
@@ -32,6 +34,11 @@ public class PacketHandler {
 		if (ADDRESS_PREVALIDATOR.matcher(message.getAddress()).matches()) {
 			ServerRedirect.setFallbackServerAddress(message.getAddress());
 		}
+		return null;
+	}
+
+	public static IMessage onAnnounceMessage(final VoidMessage message, MessageContext ctx) {
+		ServerRedirect.players.add(ctx.getServerHandler().player.getUniqueID());
 		return null;
 	}
 
@@ -56,6 +63,24 @@ public class PacketHandler {
 
 		public String getAddress() {
 			return address;
+		}
+	}
+
+	public final static class VoidMessage implements IMessage {
+		public static final VoidMessage INSTANCE = new VoidMessage();
+
+		private VoidMessage() {
+
+		}
+
+		@Override
+		public void fromBytes(ByteBuf arg0) {
+
+		}
+
+		@Override
+		public void toBytes(ByteBuf arg0) {
+
 		}
 	}
 }
