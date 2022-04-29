5c118056c15232271b734e5907f3a76f70159748
KaiKikuchi <8842403+KaiKikuchi@users.noreply.github.com>
2022-04-29 10:45:45 +0200

    Version 1.4.4

--- src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
+++ src/main/java/net/kaikk/mc/serverredirect/forge/ServerRedirect.java
@@ -46,7 +46,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ServerRedirect {
 	public static final String MODID = "serverredirect";
 	public static final String NAME = "ServerRedirect";
-	public static final String VERSION = "1.4.3";
+	public static final String VERSION = "1.4.4";
 	public static final Logger LOGGER = LogManager.getLogger();
 	protected static final Set<UUID> players = Collections.synchronizedSet(new HashSet<>());
 	@SideOnly(Side.CLIENT)
