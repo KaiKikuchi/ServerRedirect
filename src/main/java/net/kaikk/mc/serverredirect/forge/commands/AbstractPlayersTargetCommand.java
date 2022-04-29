6ab5bb44bc42315a8b8fc80c8f26130d7757d434
KaiKikuchi <8842403+KaiKikuchi@users.noreply.github.com>
2022-04-29 10:27:09 +0200

    Minor command improvements

--- src/main/java/net/kaikk/mc/serverredirect/forge/commands/AbstractPlayersTargetCommand.java
+++ src/main/java/net/kaikk/mc/serverredirect/forge/commands/AbstractPlayersTargetCommand.java
@@ -1,34 +1,31 @@
 package net.kaikk.mc.serverredirect.forge.commands;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
+import net.kaikk.mc.serverredirect.Utils;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.EntitySelector;
-import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.management.PlayerList;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
-public abstract class AbstractPlayersTargetCommand implements ICommand {
+public abstract class AbstractPlayersTargetCommand extends CommandBase {
 	public abstract void handler(EntityPlayerMP p, MinecraftServer server, ICommandSender sender, String[] args);
-	
+
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		if (!argumentsCheck(sender, args)) {
 			return;
 		}
-		
+
 		if (!checkPermission(server, sender)) {
 			sender.sendMessage(new TextComponentString("Permission denied"));
 			return;
 		}
-		
+
 		if (args[0].charAt(0) == '*') {
 			for (final EntityPlayerMP p : FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers()) {
 				handler(p, server, sender, args);
@@ -39,55 +36,18 @@ public abstract class AbstractPlayersTargetCommand implements ICommand {
 				handler(p, server, sender, args);
 			}
 		} else {
-			PlayerList pl = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList();
-			EntityPlayerMP p;
-			if (args[0].length() == 36) {
-				UUID playerId = UUID.fromString(args[0]);
-				p = pl.getPlayerByUUID(playerId);
-				if (p == null) {
-					p = pl.getPlayerByUsername(args[0]);
-				}
-			} else {
-				p = pl.getPlayerByUsername(args[0]);
-			}
-			
-			if (p == null) {
-				return;
-			}
-			
+			EntityPlayerMP p = Utils.parsePlayer(args[0]);
 			handler(p, server, sender, args);
 		}
 	}
-	
+
 	public boolean argumentsCheck(ICommandSender sender, String[] args) {
 		if (args.length < 1) {
 			sender.sendMessage(new TextComponentString(this.getUsage(sender)));
 			return false;
 		}
-		
+
 		return true;
-	}
-
-
-	@Override
-	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		return sender.canUseCommand(2, this.getName());
-	}
-
-	@Override
-	public int compareTo(ICommand o) {
-		return this.getName().compareTo(o.getName());
-	}
-
-	@Override
-	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args,
-			BlockPos targetPos) {
-		return Collections.emptyList();
-	}
-
-	@Override
-	public List<String> getAliases() {
-		return Collections.emptyList();
 	}
 
 	@Override
